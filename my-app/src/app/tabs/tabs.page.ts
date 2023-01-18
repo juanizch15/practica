74d32df9abc927c5e8683d7a4093e3b9d2885c32
juanizch15 <juanitachavez1505@gmail.com>
2023-01-18 07:09:23 -0600

    comandos git IONIC

--- my-app/src/app/tabs/tabs.page.ts
+++ my-app/src/app/tabs/tabs.page.ts
@@ -1,22 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage implements OnInit {
+export class TabsPage {
 
   constructor() {}
-
-  ngOnInit(): void {
-    console.log('Hola mundo');
-
-    let numero=20;
-    numero=100;
-    numero=10;
-    console.log(numero);
-    
-  }
 
 }
